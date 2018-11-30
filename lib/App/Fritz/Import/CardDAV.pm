3ea8fdf9a780cbf78813e5a1588d2d0f0d34f751
Max Maischein <corion@corion.net>
2018-11-30 19:16:19 +0100

    Add placeholder module

---
+++ lib/App/Fritz/Import/CardDAV.pm
@@ -0,0 +1,13 @@
+package App::Fritz::Import::CardDAV;
+use strict;
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::Fritz::Import::CardDAV - contact import scripts for the Fritz!Box
+
+=cut
